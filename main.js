e3ca9ab4768a9cdb7fcb9eaec94649e84c086172
Andrey <morozov.a_s@mail.ru>
2017-08-20 17:57:31 +0300

    v.1.1

--- main.js
+++ main.js
@@ -1,22 +1,38 @@
 /* require jQuery */
 
 function navbar(){
-
+if($('#navbar-section').offset().top < window.pageYOffset){
   for ($i = 1; $i < $('section').length+1; $i++){
   
     if($('#item-' + ($i)).offset().top < window.pageYOffset ){
     	$('nav li').removeClass('active');
       $('#sectnav-' + ($i)).addClass('active');
-      $('.active .bar').css('background', 'linear-gradient(#fff ' + ((window.pageYOffset-$('#item-' + ($i)).offset().top )/($('#item-' + ($i)).height()/100) - 1) +'%, #000 ' + ((window.pageYOffset-$('#item-' + ($i)).offset().top )/($('#item-' + ($i)).height()/100)) +'%)');
+      $height = ($('#item-' + ($i)).height()/100);
+      $('.active .bar').css('background', 'linear-gradient(#595959 ' + ((window.pageYOffset-$('#item-' + ($i)).offset().top )/$height - 1) +'%, rgba(89,89,89,0.1) ' + ((window.pageYOffset-$('#item-' + ($i)).offset().top )/$height) +'%)');
       
     }
+  }
+  }
+  else{
+  	$('nav li').removeClass('active');
   }
 }
 
 $(window).on('scroll',function(){
+
 navbar();
+
+  if($('#navbar-section').offset().top < window.pageYOffset){
+    $('#navbar').addClass('inview');
+  }else{
+    $('#navbar').removeClass('inview');
+  }
+
+
 });
 
 $('nav li').on('click', function(){
-  window.scrollTo(0, ($('#item-'+($(this).index()+1))).offset().top+1);
+ 
+  $('html, body').stop().animate({scrollTop:($('#item-'+($(this).index()+1))).offset().top+1}, 500);
+  
 });
